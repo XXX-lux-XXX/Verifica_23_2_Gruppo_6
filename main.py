7409c68913dfcf3e9b177ed20fc4fc6eb4702945
XXX_LUX_XXX <119594805+XXX-lux-XXX@users.noreply.github.com>
2023-02-23 12:57:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import Michelotto_gruppo_6
 import Foli_gruppo_6
 
-def saluto_Alessandro_Mazzolena_Gruppo_6():
+def saluto_Alessandro_Mazzolena_gruppo_6():
     print("Buongiorno, Alessandro Mazzolena")
 
 def main():
-    Michelotto_gruppo_6.saluto_Roberto_Michelotto_Gruppo_6()
-    Foli_gruppo_6.saluto_Alex_Foli_Gruppo_6()
-    saluto_Alessandro_Mazzolena_Gruppo_6()
+    Michelotto_gruppo_6.saluto_Roberto_Michelotto_gruppo_6()
+    Foli_gruppo_6.saluto_Alex_Foli_gruppo_6()
+    saluto_Alessandro_Mazzolena_gruppo_6()
 
 
 
