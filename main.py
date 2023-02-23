b53b677ccaeb65b05fd214870a3ecdc9dc783d33
XXX_LUX_XXX <119594805+XXX-lux-XXX@users.noreply.github.com>
2023-02-23 12:30:02 +0100

    Mail Leader

---
+++ main.py
@@ -0,0 +1,14 @@
+import main_Tiepo
+import main_Castellani
+
+def saluto_Alessandro_Mazzolena():
+    print("Buongiorno, Alessandro Mazzolena")
+
+def main():
+    main_Tiepo.saluto_Mattia_Tiepo()
+    main_Castellani.saluto_Lorenzo_Castellani()
+    saluto_Alessandro_Mazzolena()
+
+
+
+main()
